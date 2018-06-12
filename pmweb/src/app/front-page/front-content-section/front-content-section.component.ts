da7e868a8ad4537a6ab5bf12953afad939721fc8
quoccuong042009 <quoccuong042009@yahoo.com>
2018-06-12 22:13:11 +0700

    autoindent

--- pmweb/src/app/front-page/front-content-section/front-content-section.component.ts
+++ pmweb/src/app/front-page/front-content-section/front-content-section.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-front-content-section',
-  templateUrl: './front-content-section.component.html',
-  styleUrls: ['./front-content-section.component.css']
+	selector: 'app-front-content-section',
+	templateUrl: './front-content-section.component.html',
+	styleUrls: ['./front-content-section.component.css']
 })
 export class FrontContentSectionComponent implements OnInit {
 	selectedItem : string;
-  constructor() { }
-
-  ngOnInit() {
+	constructor() { }
+	
+	ngOnInit() {
 		this.selectedItem = "item1";
-  }
+	}
 
 }
